a6db40db9d3070a6f05a4773080e4c7caf459adc
TH9P0 <vagaluis34@gmail.com>
2025-04-04 23:25:23 -0600

    Aniado mas values a la querie de insert

--- app/src/main/java/com/example/mtoproducto/DBHelper.kt
+++ app/src/main/java/com/example/mtoproducto/DBHelper.kt
@@ -13,7 +13,16 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "mibase.db" , null,
                 "descripcion TEXT\n" +
                 ");"
         val query2 = "INSERT INTO producto VALUES (" +
-                "NULL, 'Cheetos', 17.5, NULL);"
+                "NULL, 'Cheetos', 17.5, NULL),(" +
+                "NULL, 'Doritos', 20, NULL),(" +
+                "NULL, 'Ruffles', 17, NULL),(" +
+                "NULL, 'Coca-Cola 600 ml', 22, NULL),(" +
+                "NULL, 'Cacahuates', 12, NULL),(" +
+                "NULL, 'Sprite', 17.5, NULL),(" +
+                "NULL, 'Gansito', 20, NULL),(" +
+                "NULL, 'Galletas Maria', 28.54, NULL),(" +
+                "NULL, 'Huevito Kinder', 55, NULL),(" +
+                "NULL, 'Arizona', 18, NULL);"
 
         db?.execSQL(query1)
         db?.execSQL(query2)

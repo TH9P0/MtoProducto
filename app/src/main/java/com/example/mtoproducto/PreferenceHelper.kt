12a9c9c633776537383a171790cf944886fe4b33
TH9P0 <vagaluis34@gmail.com>
2025-05-18 22:01:23 -0600

    Fix a funciones para evitar conflictos

--- app/src/main/java/com/example/mtoproducto/PreferenceHelper.kt
+++ app/src/main/java/com/example/mtoproducto/PreferenceHelper.kt
@@ -5,23 +5,11 @@ import android.content.Context
 class PreferenceHelper(context: Context) {
     private val prefs = context.getSharedPreferences("mis_preferencias", Context.MODE_PRIVATE)
 
-    /* Codigo de Preferencias de nombre
-    fun guardarNombreUsuario(nombreUsuario:String){
-        prefs.edit().putString("nombre_usuario",nombreUsuario).apply()
-    }
-
-    fun leerNombreUsuario(): String{
-        return prefs.getString("nombre_usuario","") ?: ""
-    }*/
-
     fun guardarColorUsuario(colorFavorito: String){
         prefs.edit().putString("color_favorito",colorFavorito).apply()
     }
 
-    fun leerColorUsuario(): String{
-        return prefs.getString("color_favorito","") ?: ""
-    }
-    fun obtenerColorUsuario(): String? {
-        return prefs.getString("color_favorito", null)
+    fun leerColorUsuario(): String?{
+        return prefs.getString("color_favorito",null)
     }
 }

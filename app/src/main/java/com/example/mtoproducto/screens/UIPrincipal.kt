ea3d87dbc60ea959ebefbcfc5132b5714fd094b7
TH9P0 <vagaluis34@gmail.com>
2025-05-18 22:00:34 -0600

    Limpiando ligeramente y metiendo color de usuario

--- app/src/main/java/com/example/mtoproducto/screens/UIPrincipal.kt
+++ app/src/main/java/com/example/mtoproducto/screens/UIPrincipal.kt
@@ -1,5 +1,6 @@
 package com.example.mtoproducto.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -61,6 +63,7 @@ fun UIPrincipal(navControlador: NavController){
             .fillMaxSize()
             .systemBarsPadding()
             .padding(5.dp)
+            .background(MaterialTheme.colorScheme.background)
     ) {
         Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
             Text(text="Productos Disponibles", fontSize=24.sp)
